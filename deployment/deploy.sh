a5357ed8c87cde9deae87d04383ce88b37487b24
Zack <111777752+ZandState@users.noreply.github.com>
2024-04-15 14:06:50 -0400

    Update deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,4 +6,4 @@ docker remove apacheproj
 # pull fresh image
 docker pull isolat3d/apacheimageproj4:latest
 # run new container by name, with restart
-docker run -d -p 80:5000 --name apacheproj --restart always isolat3d/apacheimageproj4:latest
+docker run -d -p 80:80 --name apacheproj --restart always isolat3d/apacheimageproj4:latest
