520eb4d9dad125aa1b5630efcf9a419142fd9c9e
Zack <111777752+ZandState@users.noreply.github.com>
2024-04-15 13:20:12 -0400

    Create deploy.sh

---
+++ deployment/deploy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# kill the old container process - stopped & then removed
+docker stop apacheproj
+docker remove apacheproj
+# pull fresh image
+docker pull isolat3d/apacheimageproj4:latest
+# run new container by name, with restart
+docker run -d -p 80:5000 --name apacheproj --restart always isolat3d/apacheimageproj4:latest
